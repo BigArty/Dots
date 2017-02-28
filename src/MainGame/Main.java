ab0fee0d938296f14c9002580fbcd638b44070d7
BigArty <artyemiy0@gmail.com>
2017-02-28 12:52:06 +0300

    Initial commit

---
+++ src/MainGame/Main.java
@@ -0,0 +1,12 @@
+package MainGame;
+
+import javax.swing.*;
+
+public class Main {
+    static JFrame f=new JFrame("Dots");
+
+    public static void main(String[] args) {
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
